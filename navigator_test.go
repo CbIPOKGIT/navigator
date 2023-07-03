a47bed6b33bb32cda815baa58fbddee97b796070
Alex <tosirok@gmail.com>
2023-07-03 15:32:00 +0300

    Navigator test function

--- navigator_test.go
+++ navigator_test.go
@@ -7,16 +7,20 @@ import (
 
 func TestGentelmanNavigator(t *testing.T) {
 	urls := []string{
-		// "https://enter.online/piscine/intex-28210-6503-l-cu-cadru-blue",
-		// "https://enter.online/piscine/bestway-56283bw-580-l-cu-cadru-blue",
+		"https://enter.online/piscine/intex-28210-6503-l-cu-cadru-blue",
+		"https://enter.online/piscine/bestway-56283bw-580-l-cu-cadru-blue",
 		"https://rozetka.com.ua/ua/buromax_bm7051_00/p354630159/",
 	}
 
 	navigator := NewNavigator(&Model{
-		Chrome:     true,
-		Visible:    true,
-		ShowImages: true,
+		Chrome:            true,
+		Visible:           true,
+		UseSystemChrome:   true,
+		ShowImages:        true,
+		ChallangeSelector: "#challenge-form",
 	})
+
+	defer navigator.Close()
 
 	for _, url := range urls {
 		err := navigator.Navigate(url)
